05c8bca552439594b8138bead35088fe82f66ffc
James Ward <james@Jamess-MacBook-Pro.local>
2019-10-27 23:03:09 -0700

    Assignment 5

--- main.py
+++ main.py
@@ -19,10 +19,15 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    # location header
+    # form data key value pair
+    quote = get_fact()
+    response = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/", data={'input_text': quote})
+    print("Response was: {}".format(response))
+    print("Response URL was: {}".format(response.request.url))
+    return "<a href='{URL}'>{URL}</a>".format(URL=response.request.url)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
